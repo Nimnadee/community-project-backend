628fafaa25006317d15539832e94e07eb6e46eac
Nimnadee <snr.nimnadi@gmail.com>
2024-12-30 07:07:56 +0530

    created order model (POST API for order model)

--- src/controller/order.controller.ts
+++ src/controller/order.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, HttpStatus, Post, Put, Res} from "@nestjs/common";
+import {Body, Controller, Get, HttpStatus, Param, Post, Put, Res} from "@nestjs/common";
 import {OrderService} from "../service/order.sevice";
 import {InventoryRequestDto} from "../model/dto/request/Inventory";
 import {Response} from "express";
@@ -7,6 +7,18 @@ import {OrderRequestDto} from "../model/dto/request/order";
 @Controller("/order")
 export class OrderController{
     constructor(private readonly orderService:OrderService) {}
+
+    @Get("/:id")
+    public async findById(@Param() params: any, @Res() response: Response) {
+        const result = await this.orderService.findById(params.id);
+        response.set(HttpStatus.OK).send(result);
+    }
+
+    @Get()
+    public async findAll(@Res() response: Response) {
+        const result = await this.orderService.findAll();
+        response.set(HttpStatus.OK).send(result);
+    }
 
     @Post()
     public async create(@Body() orderRequestDto:OrderRequestDto,@Res() response:Response){
