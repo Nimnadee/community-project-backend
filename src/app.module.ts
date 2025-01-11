f91d73f0783722737f3dde99d424bde6870a38d9
Chamith <chamithdakshina21613@gmail.com>
2025-01-11 22:49:23 +0530

    Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,11 @@ import {OrderService} from "./service/order.sevice";
 import {OrderRepository} from "./repository/order.repository";
 import {OrderMapper} from "./mapper/order.mapper";
 import {Order, OrderSchema} from "./model/schema/order";
+import { ExpensesService } from './service/Expenses.service';
+import { ExpensesMapper } from './mapper/Expenses.mapper';
+import { ExpensesRepository } from './repository/Expenses.repository';
+import { ExpensesController } from './controller/expenses.controller';
+import { Expenses, ExpensesSchema } from './model/schema/Expenses';
 
 
 @Module({
@@ -29,18 +34,24 @@ import {Order, OrderSchema} from "./model/schema/order";
 
     MongooseModule.forFeature([
       {name: Inventory.name, schema: InventorySchema},
-      {name:Order.name,schema:OrderSchema}
+      {name:Order.name,schema:OrderSchema},
+      {name:Expenses.name,schema:ExpensesSchema},
+
     ])
 
   ],
   controllers: [
-       InventoryController,
-      OrderController
+      InventoryController,
+      OrderController,
+      ExpensesController
   ],
   providers: [
-    InventoryService,
-    InventoryRepository,
-    InventoryMapper,
+      ExpensesService,
+      ExpensesMapper,
+      ExpensesRepository,
+      InventoryService,
+      InventoryRepository,
+      InventoryMapper,
       OrderService,
       OrderRepository,
       OrderMapper
